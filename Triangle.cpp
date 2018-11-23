c1cc752f63c13ea1cad3543c77385b4d1678f97a
LindforsE <lindfors.elias@gmail.com>
2018-11-23 15:28:29 +0100

    WIP circumference() position() distance()

--- Triangle.cpp
+++ Triangle.cpp
@@ -2,12 +2,21 @@
 
 Triangle::Triangle(const double * arr)
 {
-    koord = new Vector2[3];
+    coord = new Vector2[3];
+
+    coord[0].x = arr[0];
+    coord[0].y = arr[1];
+    
+    coord[1].x = arr[2];
+    coord[1].y = arr[3];
+
+    coord[2].x = arr[4];
+    coord[2].y = arr[5];
 }
 
 Triangle::~Triangle()
 {
-    delete [] koord;
+    delete [] coord;
 }
 
 //Inherited functions
@@ -24,20 +33,37 @@ double Triangle::area() const
 
 double Triangle::circumference() const
 {
-    double tmp;
-    return tmp;
+    //AB + BC + CA
+
+    //AB coord[0] -> coord[1]
+    double distAB = abs( sqrt( pow(coord[0].x - coord[1].x,2) + pow(coord[0].y - coord[1].y ,2) ) );
+    //BC coord[1] -> coord[2]
+    double distBC = abs( sqrt( pow(coord[1].x - coord[2].x,2) + pow(coord[1].y - coord[2].y ,2) ) );
+    //CA coord[2] -> coord[0]
+    double distCA = abs( sqrt( pow(coord[2].x - coord[0].x,2) + pow(coord[2].y - coord[0].y ,2) ) );
+
+    return distAB + distBC + distCA;
 }
 
 double* Triangle::position() const
 {
-    double *tmp;
+    double tmp[2];
+    
+    //( (Ax + Bx + Cx)/3 , (Ay + By + Cy)/3 )
+    tmp[0] = (coord[0].x + coord[1].x + coord[2].x)/3;
+    tmp[1] = (coord[0].y + coord[1].y + coord[2].y)/3;
+
     return tmp;
 }
 
 double Triangle::distance(Shape s) const
 {
-    double tmp;
-    return tmp;
+    double *tmp = this->position();
+    double *other = s.position();
+
+    //Distance-algorithm
+    double dist = abs( sqrt( pow(tmp[0] - other[0], 2) + pow(tmp[1] - other[1], 2) ) );
+    return dist;
 }
 
 bool Triangle::isConvex() const
